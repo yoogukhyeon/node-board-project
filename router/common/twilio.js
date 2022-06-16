6852892d89d455381255fd38fb68babbc1798b06
유국현 <rnrgus5897@gmail.com>
2022-06-16 16:14:11 +0900

    phone code

---
+++ router/common/twilio.js
@@ -0,0 +1,7 @@
+const twilio = require('twilio');
+
+//툴리오 인증
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+
+
+module.exports = twilioClient;
--- router/user.js
+++ router/user.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const twilioClient = require('./common/twilio')
+
+
 //sign-up
 router.get('/sign-up' ,async(req , res) => {
     try{
+
+        const authCode = Math.floor(100000 + Math.random() * 90000);
+
+        console.log("authCode", authCode)
+
+
+     /*    const test = await twilioClient.messages.create({
+            messagingServiceSid : process.env.TWILIO_SEND,
+            to : '+8201020595897',
+            body : `민영이 전용 핸드폰 인증 개발코드 테스트 입니다 민영이 생일이 인증번호 0519 이며 다른 인증번호 암호화 테스트 입니다~ 테스트니 삭제해라~~ㅋㅋㅋㅋ`
+        }) */
+
+      /*   console.log("test ::", test)  */
+
         res.render('login/signUp')
     }catch(err){
         console.error(err)
