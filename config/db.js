221ca1b62dcbda87af7f55f7d4c9b1e152134f25
유국현 <rnrgus5897@gmail.com>
2022-07-03 14:03:33 +0900

    mongodb change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_DEV , {
+        const conn = await mongoose.connect(process.env.MONGO_URL , {
             useNewUrlParser : true,
             useUnifiedTopology: true
         })
