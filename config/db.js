00d5805cbac29e871df499006817c7290ee7c806
yoogukhyeon <rnrgus5897@gmail.com>
2024-03-12 14:51:41 +0900

    build

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery',false);
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URL , {
