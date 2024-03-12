210d33af3aef2059cb5a6d883d3e06b2d04e6b91
yoogukhyeon <rnrgus5897@gmail.com>
2024-03-12 14:23:23 +0900

    push

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ passport.deserializeUser(function(id, done){
 passportConfig();
 
 app.set('view engine' , 'ejs');
-app.set('views' , './views');
+//app.set('views' , './views');
+app.set("views", path.join(__dirname, "views"));
 
 app.use('/' , home)
 app.use('/story' , story)
